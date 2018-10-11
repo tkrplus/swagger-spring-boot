10c01577d8bbccbb8d024ff5cf68bfe9df403c70
tkrplus <niwa.buffer@gmail.com>
2018-10-12 01:57:14 +0900

    [Step.4] API毎の情報を設定する

--- src/main/java/com/github/tkrplus/swaggerspringboot/web/sample/SampleController.java
+++ src/main/java/com/github/tkrplus/swaggerspringboot/web/sample/SampleController.java
@@ -3,6 +3,9 @@ package com.github.tkrplus.swaggerspringboot.web.sample;
 import com.github.tkrplus.swaggerspringboot.domain.sample.SampleModel;
 import com.github.tkrplus.swaggerspringboot.web.sample.form.SampleCreateForm;
 import com.github.tkrplus.swaggerspringboot.web.sample.form.SampleMergeForm;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,9 +15,11 @@ import java.util.stream.LongStream;
 
 @RestController
 @RequestMapping("/api/sample")
+@Api(tags = "サンプル", description = "サンプルの説明書き")
 public class SampleController {
 
   @GetMapping()
+  @ApiOperation(value = "サンプル一覧取得", notes = "サンプルの一覧を返却します。")
   public List<SampleModel> list() {
     final int listSize = 10;
     final List<SampleModel> list = LongStream.range(0, listSize)
@@ -25,6 +30,7 @@ public class SampleController {
   }
 
   @PostMapping
+  @ApiOperation(value = "サンプル登録", notes = "サンプルを登録します。")
   public SampleModel create(@RequestBody final SampleCreateForm form) {
     SampleModel model = new SampleModel();
     BeanUtils.copyProperties(form, model);
@@ -33,7 +39,10 @@ public class SampleController {
   }
 
   @GetMapping(value = "/{id}")
-  public SampleModel get(@PathVariable final String id) {
+  @ApiOperation(value = "サンプル取得", notes = "指定したIDのサンプルを返却します。")
+  public SampleModel get(
+      @ApiParam(name = "サンプルID", required = true, example = "178852AD-DE4E-45EF-BC01-7820C403BE9C")
+      @PathVariable("id") final String id) {
     SampleModel model = SampleModel.createDefaultModel();
     model.setId(id);
 
@@ -41,8 +50,12 @@ public class SampleController {
   }
 
   @PutMapping(value = "/{id}")
+  @ApiOperation(value = "サンプル更新",
+      notes = "指定したIDのサンプルを更新します。\n" +
+          "登録されていない場合は、登録を行います。")
   public SampleModel merge(
-      @PathVariable final String id,
+      @ApiParam(name = "サンプルID", required = true, example = "178852AD-DE4E-45EF-BC01-7820C403BE9C")
+      @PathVariable("id") final String id,
       @RequestBody final SampleMergeForm form) {
     SampleModel model = new SampleModel();
     BeanUtils.copyProperties(form, model);
@@ -52,7 +65,10 @@ public class SampleController {
   }
 
   @DeleteMapping(value = "/{id}")
-  public SampleModel delete(@PathVariable final String id) {
+  @ApiOperation(value = "サンプル削除", notes = "指定したIDのサンプルを削除します。")
+  public SampleModel delete(
+      @ApiParam(name = "サンプルID", required = true, example = "178852AD-DE4E-45EF-BC01-7820C403BE9C")
+      @PathVariable("id") final String id) {
     SampleModel model = SampleModel.createDefaultModel();
     model.setId(id);
 
--- src/main/java/com/github/tkrplus/swaggerspringboot/web/sample/form/SampleCreateForm.java
+++ src/main/java/com/github/tkrplus/swaggerspringboot/web/sample/form/SampleCreateForm.java
@@ -1,13 +1,21 @@
 package com.github.tkrplus.swaggerspringboot.web.sample.form;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(value = "サンプル登録用フォーム")
 public class SampleCreateForm {
 
+  @ApiModelProperty(name = "サンプルID", required = true, example = "178852AD-DE4E-45EF-BC01-7820C403BE9C")
   private String id;
 
+  @ApiModelProperty(name = "サンプル名", required = true, example = "ほげほげのサンプル")
   private String name;
 
+  @ApiModelProperty(name = "数", example = "255", allowableValues = "range[1, 5]")
   private long count;
 
+  @ApiModelProperty(name = "フラグ", required = true, example = "false")
   private boolean flag;
 
   public String getId() {
